9970dc3243d1525e2a6f09a21b7d3322a82bbfb9
Nikhil yadav <64726741+nikhc@users.noreply.github.com>
2023-09-02 04:39:16 +0530

    mm

--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -1,53 +1,44 @@
+const nodemailer = require('nodemailer');
 
-const nodemailer = require("nodemailer");
-
-module.exports.sendMail=async function sendMail(str,data) {
-
+// Create a Nodemailer transporter using Gmail SMTP
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: 'smtp.gmail.com',
   port: 587,
   secure: false,
   auth: {
-    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-    user: 'nikhil23fbd@gmail.com',
-    pass: "rbjmvcyelvadmdav"
-  }
-  
+    user: 'your@gmail.com', // Your Gmail email address
+    pass: 'your-password', // Your Gmail password (consider using an app password for security)
+  },
 });
-console.log("dhjsbjhbdsjhbc")
 
-// async..await is not allowed in global scope, must use a wrapper
+module.exports.sendMail = async function sendMail(str, data) {
+  try {
+    var Osubject, Ohtml;
 
-    var Osubject,Otext,Ohtml;
-    if(str=="signup"){
-        Osubject=`thankyou for signning ${data.name}`
-        Ohtml=`<h1> bro welcome</h1>
-        name-${data.name}`
-        console.log("mnds jh jhsds jbjdsbcjbds")
+    if (str === 'signup') {
+      Osubject = `Thank you for signing up, ${data.name}`;
+      Ohtml = `<h1>Welcome, ${data.name}!</h1>`;
+    } else if (str === 'resetPassword') {
+      Osubject = 'Reset Password';
+      Ohtml = `<h1>Food App</h1><p>${data.resetPasswordLink}</p>`;
     }
-    else if(str=='resetPassword'){
-        Osubject=`resetpassword`
-        console.log("else")
 
-        Ohtml=`<h1>food app</h1> ${data.resetPasswordLink}`
+    // Construct the mailOptions object
+    const mailOptions = {
+      from: 'your@gmail.com', // Replace with your Gmail email address
+      to: data.email,
+      subject: Osubject,
+      html: Ohtml,
+    };
 
-    }
-  // send mil with defined transport object
-  const info = await transporter.sendMail({
-    from: '"nikhil" <nikhil23fbd@gmail.com>', // sender address
-    to: data.email, // list of receivers
-    subject: Osubject, // Subject line
-    // plain text body
-    html: Ohtml // html body
-  });
+    // Send the email using the Gmail SMTP transporter
+    const info = await transporter.sendMail(mailOptions);
 
-  
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log('Email sent: ' + info.response);
+  } catch (err) {
+    console.error('Error sending email:', err);
+    // Handle the error as needed
+  }
+};
 
-  //
-  // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-  //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-  //       <https://github.com/forwardemail/preview-email>
-  //
-}
 
